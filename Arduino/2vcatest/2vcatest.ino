a2e477c7d504725420a37b5f6aa9077196fab087
Youngrae Kim <dufo7070@kaist.ac.kr>
2024-08-23 14:28:18 +0900

    ADD: Import Ardity for arduino connection

---
+++ Arduino/2vcatest/2vcatest.ino
@@ -0,0 +1,115 @@
+#include <Wire.h>
+#include "Haptic_Driver.h"
+
+#define NUMBER_OF_SENSORS 2
+Haptic_Driver hapDrive[NUMBER_OF_SENSORS];
+
+void TCA9548A(uint8_t bus){
+  Wire.beginTransmission(0x70);  // TCA9548A address
+  Wire.write(1 << bus);          // send byte to select bus
+  Wire.endTransmission();
+  //Serial.print(bus);
+}
+
+hapticSettings lfi;
+
+void setup() {
+  // put your setup code here, to run once:
+  //start
+  Wire.begin();
+  Serial.begin(115200);
+
+  lfi.motorType = LRA_TYPE;
+  lfi.absVolt = 3.5;
+  lfi.nomVolt = 2.47;
+  lfi.currMax = 295.1;
+  lfi.impedance = 8.37;
+  lfi.lraFreq = 80;
+
+  for(int i; i<NUMBER_OF_SENSORS; i++){
+    TCA9548A(i);
+    hapDrive[i].begin();
+    if( !hapDrive[i].begin())
+      Serial.println("Could not communicate with Haptic Driver1.");
+    hapDrive[i].enableFreqTrack(false);
+    hapDrive[i].setOperationMode(DRO_MODE);
+    if(!hapDrive[i].setMotor(lfi))
+    {
+      Serial.println("Setting failed");
+    }
+    hapDrive[i].setVibrate(0);
+  }
+}
+
+//just in case
+int event = 0; 
+
+//motors
+int indEx = 0;
+int indFlex = 1;
+
+char buf[3];
+
+void copy(int* src, int* dst, int len) {
+    memcpy(dst, src, sizeof(src[0])*len);
+}
+
+void loop() {
+  for(int i; i<NUMBER_OF_SENSORS; i++){
+    event = hapDrive[i].getIrqEvent();
+    hapDrive[i].clearIrq(event);
+  }
+
+  if (Serial.available() >= 2){
+    //from unity
+    // Serial.readBytes(buf, 3);
+    // uint8_t setting = int(buf[0]);
+    // uint8_t command = int(buf[1]);
+    // uint8_t duration = int(buf[2]);
+
+    //manual check
+    int setting = Serial.parseInt();
+    int command = Serial.parseInt();
+    int duration = Serial.parseInt();
+
+    Serial.print("Setting read: ");
+    Serial.println(setting);
+    Serial.print("Command read: ");
+    Serial.println(command);
+
+    if (setting == 0){
+      return;
+    }
+
+    int amp = 127;
+    if (command == 0){
+      amp = 0;
+    }
+
+    int targetMotor;
+
+    switch(setting){
+      case 1:
+        targetMotor = indEx;
+        break;
+      case 2:
+        targetMotor = indFlex;
+        break;
+    }
+    
+    Serial.print("Target Motors: ");
+    Serial.println(targetMotor);
+
+    Serial.print("Vibrating with amplitude: ");
+    Serial.println(amp);
+
+    TCA9548A(targetMotor);
+    hapDrive[targetMotor].setVibrate(amp);
+    if(command == 2){
+      Serial.print("Vibrating with duration(ms): ");
+      Serial.println(duration*250);
+      delay(duration*250);
+      hapDrive[targetMotor].setVibrate(0);
+    }
+  }
+}
--- Assets/Scripts/Gripper Scene/GripperManager.cs
+++ Assets/Scripts/Gripper Scene/GripperManager.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.XR.Hands;
 
-public class StaplerManager : MonoBehaviour
+public class GripperManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject staplerBracket;
+    private GameObject leftHandle;
     
     [SerializeField]
     private JointManager jointManager;
@@ -15,10 +14,10 @@ public class StaplerManager : MonoBehaviour
     public TextMeshProUGUI grabbingText;
     public TextMeshProUGUI holdingText;
 
-    private float currentHingeAngle = 0f;
+    private float currentAngle = 10f;
     private readonly float rotationSpeed = 1.2f;
-    private readonly float staplerRadius = 0.51631709605f;
-
+    private readonly float gripperRadius = 0.51631709605f;
+    private Vector3 originalEulerAngles;
 
     private bool grabbing = false;
     private bool holding = false;
@@ -33,29 +32,33 @@ public class StaplerManager : MonoBehaviour
         set { holding = value; }
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalEulerAngles = leftHandle.transform.localEulerAngles;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // Fetching hands, if there exist and left one is holding stapler then transform the stapler
+                // Fetching hands, if there exist and left one is holding stapler then transform the stapler
         if (holding) 
         {
             // Calculate the euler angle of stapler based on the distance between index distal and thumb tip
-            var (updatedAngle, _) = jointManager.CalculateAngle(currentHingeAngle, staplerRadius);
-            float updatedHingeAngle = 9 - updatedAngle;
-            float deltaAngle = updatedHingeAngle - currentHingeAngle;
-            
+            var (updatedAngle, deltaAngle) = jointManager.CalculateAngle(currentAngle, gripperRadius);
+
             // Rotate the bracket and stabilize the whole stapler
-            staplerBracket.transform.Rotate(0, 0, deltaAngle * rotationSpeed);
-            currentHingeAngle = updatedHingeAngle;
+            leftHandle.transform.RotateAround(leftHandle.transform.position, Vector3.forward, deltaAngle * rotationSpeed);
+            currentAngle = updatedAngle;
         }
 
         grabbingText.text = "Grabbing: " + Grabbing.ToString();
         holdingText.text = "Holding: " + Holding.ToString();
     }
 
-    public void ResetStapler()
+    public void ResetGripper()
     {
-        float deltaAngle = 0 - currentHingeAngle;
-        staplerBracket.transform.Rotate(0, 0, deltaAngle * rotationSpeed);
+        leftHandle.transform.localEulerAngles = originalEulerAngles;
+        currentAngle = 10f;
     }
 }
